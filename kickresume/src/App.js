969803465f3e64c12b8d908d408647f1d19ffe79
revanth-026 <revanthnaidu_bevara@srmap.edu.in>
2025-06-30 11:45:30 +0000

    Your message

---
+++ kickresume/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-4xl text-blue-600 font-bold text-center mt-10">
+      ✅ KickResume + Tailwind CSS is working!
+    </div>
+  );
+}
+
+export default App;
